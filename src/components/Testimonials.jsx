74bcea9d595c53855ae5eff121299eb36d30bb15
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-06 23:15:58 -0600

    Completed Gallery section

---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,21 @@
+import react from 'react'
+import {Row, Col, Container} from 'react-bootstrap'
+
+
+
+const Testimonials = () => {
+    return(
+        <div>
+            <Container>
+  <Row md={4}>
+    <Col>1 of 3</Col>
+    <Col xs={6}>2 of 3</Col>
+    <Col>3 of 3</Col>
+  </Row>
+</Container>
+        </div>
+    )
+}
+
+
+export default Testimonials
