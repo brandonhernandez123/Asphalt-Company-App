4af5e87c32a021ef9fa058448bb53e85a20dc562
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-08 23:54:47 -0600

    working on testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,5 +1,6 @@
 import react from 'react'
-import {Row, Col, Container, Image, Figure} from 'react-bootstrap'
+import {Row, Col, Container, Image, Figure, Stack} from 'react-bootstrap'
+import { Avatar } from '@mui/material'
 
 
 
@@ -12,32 +13,37 @@ const Testimonials = () => {
                 <h1>TESTIMONIALS</h1>
                     <Row>
                         <Col>
-                        <Figure>
-  <Figure.Image
-    width={171}
-    height={180}
-    alt="171x180"
-    src="https://media.istockphoto.com/photos/portrait-of-a-young-man-outdoors-smiling-picture-id1135381173?k=20&m=1135381173&s=612x612&w=0&h=pZEZ7V-VbBOTSekv7rlKhjtQP5ZyxDQ3YDYN-dofHmk="
-  />
-  <Figure.Caption>  
-      <p fluid className='review'>""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.""</p>
+                        <Stack direction="horizontal" gap={3}>
+  <div>
+  <Avatar
+  alt="Remy Sharp"
+  src="https://media.istockphoto.com/photos/portrait-of-a-young-man-outdoors-smiling-picture-id1135381173?k=20&m=1135381173&s=612x612&w=0&h=pZEZ7V-VbBOTSekv7rlKhjtQP5ZyxDQ3YDYN-dofHmk="
+  sx={{ width: 56, height: 56 }}
+/>
 
-  </Figure.Caption>
-</Figure>
+  </div>
+  <div className=" ms-auto">      <p fluid className='review'>""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at."" <span className='owner'>- John Doe</span></p>
+</div>
+  <div className="vr" />
+  
+</Stack>
     </Col>
     <Col>
-    <Figure>
-  <Figure.Image
-    width={171}
-    height={180}
-    alt="171x180"
-    src="https://media.istockphoto.com/photos/portrait-of-a-young-man-outdoors-smiling-picture-id1135381173?k=20&m=1135381173&s=612x612&w=0&h=pZEZ7V-VbBOTSekv7rlKhjtQP5ZyxDQ3YDYN-dofHmk="
-  />
-  <Figure.Caption>  
-      <p fluid className='review'>""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at.""</p>
+    <Stack direction="horizontal" gap={3}>
+  <div>
+  <Avatar
+  alt="Remy Sharp"
+  src="https://media.istockphoto.com/photos/portrait-of-a-young-man-outdoors-smiling-picture-id1135381173?k=20&m=1135381173&s=612x612&w=0&h=pZEZ7V-VbBOTSekv7rlKhjtQP5ZyxDQ3YDYN-dofHmk="
+  sx={{ width: 56, height: 56 }}
+/>
 
-  </Figure.Caption>
-</Figure>
+  </div>
+  <div className=" ms-auto">      <p fluid className='review'>""Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sedasd commodo nibh ante facilisis bibendum dolor feugiat at."" <span className='owner'>- John Doe</span></p>
+</div>
+  <div className="vr" />
+  
+</Stack>
+   
     </Col>
     </Row>
 
