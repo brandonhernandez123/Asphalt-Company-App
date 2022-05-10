90f60f07dfbd8fefac45b27be692ae6923c167d7
Brandon Hernandez <brandoncinthia0621@gmail.com>
2022-05-10 15:29:37 -0600

    recreated testimonials page and fixed about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,16 +8,44 @@ const About = () => {
         <div className='about'>
             <Container fluid>
   <Row>
-<Col fluid>
- <h2 className='aboutus2'>ABOUT US</h2>
- <p className='aboutp'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Tellus elementum sagittis vitae et leo. Porta non pulvinar neque laoreet suspendisse. Tempus egestas sed sed risus. Egestas congue quisque egestas diam in arcu cursus euismod. Nulla porttitor massa id neque aliquam vestibulum morbi blandit cursus. Ante in nibh mauris cursus mattis molestie a iaculis at. Tellus cras adipiscing enim eu. Aenean vel elit scelerisque mauris pellentesque pulvinar. Netus et malesuada fames ac turpis egestas maecenas pharetra convallis. Est ullamcorper eget nulla facilisi etiam. Laoreet id donec ultrices tincidunt. Sapien nec sagittis aliquam malesuada bibendum.</p>
 
-        
-    </Col>
-    
+   
+        <h2 className='aboutus2'>ABOUT US</h2>
+        <p className='fill'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+</Row>
     <br/>
-  </Row>
- <Link
+    <Row>
+        <h2>WHY CHOOSE US</h2>
+        <Col>
+            <ul className='chooseus'>
+                <li>
+                  Lorem Ipsum Dolor
+                </li>
+                <li>
+                  Lorem Ipsum Dolor
+                </li>
+                <li>
+                  Lorem Ipsum Dolor
+                </li>
+            </ul>
+        </Col>
+        <Col>
+            <ul className='chooseus'>
+            <li>
+                  Lorem Ipsum Dolor
+                </li>
+                <li>
+                  Lorem Ipsum Dolor
+                </li>
+                <li>
+                  Lorem Ipsum Dolor
+                </li>
+            </ul>
+        </Col>
+    </Row>
+    <br/>
+  
+<Link
           activeClass="active"
           to="section6"
          spy={true}
